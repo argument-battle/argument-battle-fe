6a9c1be81be564ccc7938f27e5830b0decdc4dd0
niqqq25 <andriusrad@gmail.com>
2020-03-14 23:24:42 +0200

    feat: AB-43 add create user api

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 const Router = require('express').Router;
 
-const { getAll } = require('../services/user');
+const { getAll, create } = require('../services/user');
 
 const router = new Router();
 
 router.get('/', getAll);
+router.post('/', create);
 
 module.exports = router;
--- server/services/user.js
+++ server/services/user.js
@@ -1,8 +1,24 @@
 const { User } = require('../models');
+const bcrypt = require('bcryptjs');
 
 async function getAll(req, res) {
     const users = await User.findAll({ raw: true });
     res.status(200).send({ users });
 }
 
-module.exports = { getAll };
+async function create(req, res) {
+    try {
+        const salt = 10;
+        const user = req.body;
+
+        const hash = await bcrypt.hash(user.password, salt);
+        user.password = hash;
+
+        await User.create(user);
+        res.status(201).send({ message: 'Success' });
+    } catch (error) {
+        res.status(500).send({ error });
+    }
+}
+
+module.exports = { getAll, create };
