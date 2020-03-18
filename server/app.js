af940aaca88e44c2d78f33c7637b524b37bdf367
Andrius Radzivonas <32072309+niqqq25@users.noreply.github.com>
2020-03-19 00:04:50 +0200

    migrate to mongoose (#8)

--- server/app.js
+++ server/app.js
@@ -3,10 +3,18 @@ const router = require('./routes/router');
 const cookieParser = require('cookie-parser');
 const Bundler = require('parcel-bundler');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const isProd = process.env.NODE_ENV === 'production';
 
 const app = express();
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 const entryFiles = path.join(__dirname, '../public/index.html');
 const bundler = new Bundler(entryFiles, { sourceMaps: !isProd });
