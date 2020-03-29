fbbb0568b9f5356a23b735afdb71ed7c9b9434a8
voidpumpkin <32368314+voidpumpkin@users.noreply.github.com>
2020-03-29 19:17:39 +0300

    fix user error

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const { getAll, getGuest, getMe, get, create, login, logout } = require('../services/user');
 const getUserMiddleware = require('../middleware/getUserMiddleware');
+const authUserMiddleware = require('../middleware/authUserMiddleware');
 
 const router = new Router();
 
 router.get('/guest', getGuest);
-router.get('/me', getUserMiddleware, getMe);
+router.get('/me', authUserMiddleware, getUserMiddleware, getMe);
 router.get('/:id', get);
 router.get('/', getAll);
 router.post('/', create);
